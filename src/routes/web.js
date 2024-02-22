5239b93ae38fe5ad9411d47fe045d76cc4969cdb
Tung Do <tungsibolo2003@gmail.com>
2024-02-22 13:58:51 +0700

    commit 1

---
+++ src/routes/web.js
@@ -0,0 +1,18 @@
+import express from "express";
+import homeController from "../controller/homeController";
+
+const router = express.Router();
+
+/**
+ *
+ * @param {*} app : express app
+ *
+ */
+const initWebRoutes = (app) => {
+  router.get("/", homeController.handleHelloWord);
+  router.get("/user", homeController.handleUserPage);
+
+  return app.use("/", router);
+};
+
+export default initWebRoutes;
